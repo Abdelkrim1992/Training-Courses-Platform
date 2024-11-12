098ccd5286a3ad918790a3bdc87031f73e5413ac
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2024-11-12 08:11:48 +0000

    the final version

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ import './bootstrap';
 import {createApp} from 'vue';
 import App from './components/App.vue';
 import Router from './routes/Router.js';
-import '@fortawesome/fontawesome-free/css/all.css';
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 import 'froala-editor/js/plugins.pkgd.min.js';
