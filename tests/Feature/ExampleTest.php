0cc86e00860eefca4df1cfc6061e05b1c95e6411
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2024-12-26 13:29:03 +0000

    update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,7 +11,7 @@ class ExampleTest extends TestCase
 
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
+        $response = $this->get('/');  // Using the get() method provided by TestCase class
     
         $response->assertStatus(200);
     }
