878672b551fc6ada48b564b8abad075a1ad2fea0
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2024-12-26 14:31:16 +0000

    update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
@@ -12,7 +12,7 @@ class ExampleTest extends TestCase
 
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/Login');  // Using the get() method provided by TestCase class
+        $response = $this->get('/Login'); 
     
         $response->assertStatus(200);
     }
