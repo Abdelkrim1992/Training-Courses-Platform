3f8d69bee22fc3ad7691efb74a5d8f5cdc9157c8
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2024-12-26 13:29:16 +0000

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,6 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Facade;
-use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
