59d96b88efd3343fed8fd6b67d5f96497515155c
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2024-12-26 13:25:05 +0000

    update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,21 +2,17 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use RefreshDatabase; // Make sure this is included
+    use RefreshDatabase;
 
-    /**
-     * A basic test example.
-     */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');  // Ensure this URL is valid in the test environment
+        $response = $this->get('/');
     
         $response->assertStatus(200);
     }
-
 }
