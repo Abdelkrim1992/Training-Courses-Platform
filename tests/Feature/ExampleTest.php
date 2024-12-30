46d4e90636c330765a2af3d1bd468c776590a15f
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2024-12-30 18:54:35 +0000

    update ci cd

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,5 +10,7 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/test'); 
+    
+        $response->assertStatus(200);
     }
 }
