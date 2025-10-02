7804c355ab43bfd8722d402d3a302570b8792f51
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-10-02 23:16:02 +0100

    update gitignore and cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://training-courses-backend-6jkbrp.laravel.cloud'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
