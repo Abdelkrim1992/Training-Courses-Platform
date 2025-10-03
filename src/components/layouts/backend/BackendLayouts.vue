41bff92b6ae4714ccb85abde218f33290c17f1bc
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-10-03 02:30:48 +0100

    update frontend

--- src/components/layouts/backend/BackendLayouts.vue
+++ src/components/layouts/backend/BackendLayouts.vue
@@ -3,14 +3,6 @@
 import Header from '../backend/Header.vue';
 import Sidebar from '../backend/Sidebar.vue';
 
-import "@/assets/backend/css/style.css"
-// Backend JS assets (order matters)
-import "@/assets/backend/js/plugins/popper.min.js"
-import "@/assets/backend/js/plugins/bootstrap.min.js"
-import "@/assets/backend/js/plugins/simplebar.min.js"
-import "@/assets/backend/js/plugins/feather.min.js"
-import "@/assets/backend/js/plugins/datepicker-full.min.js"
-import "@/assets/backend/js/fonts/custom-font.js"
 
 
 </script>
