f13d94b3d79982571a30fbfec9688f8d5039ad99
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-10-03 02:38:20 +0100

    update backend cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'https://training-courses-backend-6jkbrp.laravel.cloud'],
+    'allowed_origins' => ['http://localhost:5173', 'https://training-courses-backend-6jkbrp.laravel.cloud', 'https://training-courses-platform.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
